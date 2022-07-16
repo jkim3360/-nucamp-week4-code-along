3c4a0304733293c30af9bb93742b1536c6847486
James Kim <jamesdaehokim@gmail.com>
2022-07-16 00:49:35 -0400

    update week 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,206 +1,76 @@
-function runFunction() {
-  alert('The button has been clicked!')
+/* --------------------- FOR LOOP PRACTICE --------------------- */
 
-  // Uncomment the line of code below to log a message to the console instead
-  // Press command + option + j on Mac to open the console
-  // Press ctrl + shift + j on Windows to open the console
-  // console.log('Function ran successfully')
+// Finish the code below to print out the correct iteration
+// e.g. first log should say "This is iteration number 1"
+// e.g. second log should say "This is iteration number 2" etc.
 
-  // Reassigning let variable
-  // let name = null
-  // const nameInSpan = document.getElementById('name').textContent
-  // name = nameInSpan
-  // console.log(name)
+for (let i = 0; i < 10; i++) {
+  console.log('This is iteration number')
 }
 
-/* --------------------- VARIABLE PRACTICE --------------------- */
+/* --------------------- FOR OF LOOP PRACTICE --------------------- */
 
-function variablePractice() {
-  const data = null
-  const myNum = 1
-  const myString = 'foo'
-  const myString2 = '5'
-  const myBoolean = true
-  const myBoolean2 = false
-  console.log(data, myNum, myString, myString2, myBoolean, myBoolean2)
+// 1. Declare a variable and initialize it with an array containing five values of any data type
 
-  // let allows variables to be initialized before being assigned a value:
-  let car
-  car = 'Mustang'
-  console.log(car)
+// 2. Write a for... of loop and print the values to the console
 
-  // Reassigning variables
-  car = 'Ferrari'
-  console.log(car)
+/* --------------------- ARRAY METHOD PRACTICE --------------------- */
+
+// 1. Use the array from the previous exercise and remove the first value from the beginning
+// 2. Add a new value to the end of the array
+// 3. Loop through the values and print to screen
+// 4. Call the function
+
+function practiceArrMethods() {}
+
+/* --------------------- MORE ARRAY METHODS --------------------- */
+
+// 1. Concatenate the arrays
+// 2. Reverse the array
+// 3. Join the string with ''
+// 4. Print the result to the console
+// 4. Call the function
+
+const arr1 = ['t', 'p', 'i', 'r', 'c', 's', 'a', 'v', 'a', 'j', ' ', 'e']
+const arr2 = ['v', 'o', 'l', ' ', 'I']
+
+function concatArr() {
+  // console.log(arr1.concat(arr2).reverse().join(''))
 }
 
-// variablePractice()
+// Copy the code for the splice and slice methods below and print the results to the console.
 
-/* --------------------- FUNCTIONS PRACTICE --------------------- */
+/* --------------------- STRING LOOP--------------------- */
 
-// 1. Finish the sayHello function below and call the function with an argument.
+// Declare a variable and initialize it with a string of your choice.
+// Using a for... of loop, loop through each value and print it to the console.
 
-function sayHello() {}
+/* --------------------- DOM MANIPULATION & MOUSE EVENTS --------------------- */
 
-// 2. Write your own function declaration below that logs a message to the console with multiple parameters
+/* Print contents of an HTML to the console.
+1. Declare a variable
+2. Assign the element with an ID in index.html to the variable
+3. Print its contents to the console (hint: Google innerText)
+*/
 
-/* --------------------- SCOPE PRACTICE --------------------- */
+/* Add a button to the document and remove any node when it is clicked */
 
-function scopePractice() {
-  // var is function scoped
-  // Variables initialized with var are available anywhere witin function regardless of scope.
-  var name1 = 'Frank'
-  // let name4 = 'Michelle'
+// document.querySelector('button').addEventListener('click', ()=>{
+//   document.getElementById('heading').remove()
+// })
 
-  function innerFunction() {
-    // Variables initialized with let and const are block scoped
-    // Block scoped variables are only available within this block of code (block refers to curly braces)
-    let name2 = 'John'
-    const name3 = 'Kevin'
+/* --------------------- SET TIMEOUT AND SET INTERVAL --------------------- */
 
-    console.log(name2, name3, name4)
-  }
+// Log any string to the console using setTimeout.
 
-  innerFunction()
+// Log any data type to the console every 2 seconds using setInterval.
 
-  // Uncomment the line of code to below to test variables availability.
-  // console.log(name2, name3)
+const myInterval = setInterval(() => {
+  console.log('This is a data type.')
+}, 2000)
+
+// Add another button to the document and clear the interval on click.
+
+const stop = () => {
+  clearInterval(myInterval)
 }
-
-// scopePractice()
-
-/* --------------------- IF ... ELSE IF ... ELSE / COMPARISON OPERATORS PRACTICE --------------------- */
-
-// Complete the conditions below
-
-// function isLegalAge(age) {
-//   if () {
-//     console.log('No age entered.')
-//   } else if () {
-//     console.log('Legal')
-//   } else {
-//     console.log('Illegal')
-//   }
-// }
-
-// isLegalAge()
-
-/* --------------------- LOGICAL OPERATORS PRACTICE --------------------- */
-
-// Returns first falsy value or last truthy value
-
-// False Values List:
-// - false
-// - null
-// - undefined
-// - empty string: "" and ''
-// - 0
-// - NaN
-
-const hasEngine = true
-const hasWheels = true
-
-// Uncomment the below if else block
-// if (hasEngine && hasWheels) {
-//     console.log('This car can drive!')
-// } else {
-//     console.log('I need a mechanic.')
-// }
-
-// Play around with the order of truthy and falsy values below and see what is printed to the console!
-// console.log(0 && true)
-
-const hasCreditCard = false
-const hasDebitCard = false
-
-// Uncomment the below if else block
-// if (hasCreditCard || hasDebitCard) {
-//     console.log('Have dinner.')
-// } else {
-//     console.log('Go hungry.')
-// }
-
-// Play around with the order of truthy and falsy values below and see what is printed to the console!
-// console.log(false || 1)
-
-/* --------------------- SWITCH PRACTICE --------------------- */
-
-const workLoad = ''
-
-// Add a string to each case that is appropriate for the message in each console log.
-// switch (workLoad) {
-//   case '':
-//     console.log("I'm stressed.")
-//     break
-//   case '':
-//     console.log('I can handle this.')
-//     break
-//   case '':
-//     console.log('Life is good :D')
-//   default:
-//     console.log('I need a job.')
-// }
-
-/* --------------------- BINARY AND UNARY OPERATORS PRACTICE --------------------- */
-
-// let number = 10
-
-// Add 5 to number using binary a operator (+=)
-// console.log()
-// Subtract 3 from number using a binary operator (-=)
-// console.log()
-
-// let counter = 37
-
-// Using unary operators (++ and --), increment counter twice and decrement once.
-
-// console.log(counter)
-
-/* --------------------- WHILE LOOP PRACTICE --------------------- */
-
-let i = 0
-
-// Add a condition to the while loop that will stop the loop at a certain integer.
-// function whileLoop() {
-//   while () {
-//     i += 1
-//     console.log('i is', i)
-//   }
-// }
-
-// whileLoop ()
-
-/* --------------------- ARRAYS PRACTICE --------------------- */
-
-// Array methods list
-// .unshift()
-// .shift()
-// .pop()
-// .push()
-// .join()
-// .includes()
-// .indexOf()
-
-const randomPeople = [
-  'Philip Seymour Hoffman',
-  'Jack White',
-  'Samson',
-  'Delilah',
-  'Will Ferrell'
-]
-
-// 1. Remove a value from the end of the array.
-
-// 2. Remove a value from the beginning of the array.
-
-// 3. Add a value to the end of the array.
-
-// 4. Combine the array in one string, separating each name with by a comma.
-
-// 5. Check to see if a value exists in the array.
-
-/* 6. Declare a variable and initialize it with array of any five values of your choice. 
-   Write a function that takes in the array as an argument and prints any one of the above 
-   challenges to the console. */
-
-// console.log(randomPeople)
